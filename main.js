a463564885f40c7ba08933c43f60bac0d9db3aa1
Fred Gohsman <102371320+fredgohsman@users.noreply.github.com>
2022-12-05 12:21:25 -0500

    remove table

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ const run = async () => {
     const name = `${lastName.slice(0, 3)}${firstName.slice(0, 2).toLowerCase()} ${maidenName.slice(0, 2)}${birthTown.slice(0, 3).toLowerCase()}`
     
     // Add GitHub Summary Log Entry
-    /* await core.summary
+    await core.summary
         .addHeading('Star Wars Name Generator')
-        .addTable([
-            [{ data: 'First name', header: true }, { data: 'Last name', header: true }, { data: 'Birth town', header: true }, { data: "Mother''s Maiden Name", header: true }]
-            [firstName, lastName, birthTown, maidenName]
-        ])
+        //.addTable([
+        //    [{ data: 'First name', header: true }, { data: 'Last name', header: true }, { data: 'Birth town', header: true }, { data: "Mother''s Maiden Name", header: true }]
+        //    [firstName, lastName, birthTown, maidenName]
+        //])
         .addHeading(name, 2)
-        .write() */
+        .write()
 
     // Save the name to an output variable
     core.setOutput('StarWarsName', name)
