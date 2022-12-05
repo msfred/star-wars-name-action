c79685027a8175038eea14046d6559000232a3b3
Fred Gohsman <102371320+fredgohsman@users.noreply.github.com>
2022-12-05 10:05:32 -0500

    add summary log

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 import core from '@actions/core'
 
-const run = () => {
+const run = async () => {
     const firstName = core.getInput('FirstName')
     const lastName = core.getInput('LastName')
     const birthTown = core.getInput('BirthTown')
     const maidenName = core.getInput('MaidenName')
 
-    const name = `${lastName.slice(0,3)}${firstName.slice(0,2).toLowerCase()} ${maidenName.slice(0,2)}${birthTown.slice(0,3).toLowerCase()}`
+    const name = `${lastName.slice(0, 3)}${firstName.slice(0, 2).toLowerCase()} ${maidenName.slice(0, 2)}${birthTown.slice(0, 3).toLowerCase()}`
     core.setOutput('StarWarsName', name)
+
+    // Add GitHub Summary Log Entry
+    await core.summary
+        .addHeading('Star Wars Name Generator')
+        .addTable([
+            [{ data: 'First name', header: true }, { data: 'Last name', header: true }, { data: 'Birth town', header: true }, { data: "Mother''s Maiden Name", header: true }]
+            [firstName, lastName, birthTown, maidenName]
+        ])
+        .addHeading(name, 2)
+        .write()
 }
 
 run()
